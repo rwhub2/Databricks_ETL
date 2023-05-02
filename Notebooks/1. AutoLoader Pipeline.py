260a14f7fe60b2456739132d3f483e040409f532
rwhub2 <rwhub2>
2023-05-02 10:57:27 +0000

    PI

--- Notebooks/1. AutoLoader Pipeline.py
+++ Notebooks/1. AutoLoader Pipeline.py
@@ -1,7 +1,7 @@
 # Databricks notebook source
 # MAGIC %md
 # MAGIC
-# MAGIC ### Simple Pipeline - Medallion Architecture
+# MAGIC ### AutoLoader Pipeline - Medallion Architecture
 
 # COMMAND ----------
 
@@ -115,81 +115,79 @@ display(df1.limit(2))
 # MAGIC %sql
 # MAGIC
 # MAGIC CREATE TABLE IF NOT EXISTS dimCustomer (
-# MAGIC   CustomerKey STRING NOT NULL,
-# MAGIC 	GeographyKey STRING,
+# MAGIC   CustomerKey INT NOT NULL,
+# MAGIC 	GeographyKey INT,
 # MAGIC 	CustomerAlternateKey STRING, 
 # MAGIC 	Title STRING,
 # MAGIC 	FirstName STRING,
 # MAGIC 	MiddleName STRING,
 # MAGIC 	LastName STRING,
-# MAGIC 	NameStyle STRING,
-# MAGIC 	BirthDate STRING,
+# MAGIC 	NameStyle INT,
+# MAGIC 	BirthDate DATE,
 # MAGIC 	MaritalStatus STRING,
 # MAGIC 	Suffix STRING,
 # MAGIC 	Gender STRING,
-# MAGIC 	EmailAddress STRING,
-# MAGIC 	YearlyIncome STRING,
-# MAGIC 	TotalChildren STRING,
-# MAGIC 	NumberChildrenAtHome STRING,
-# MAGIC 	EnglishEducation STRING,
-# MAGIC 	SpanishEducation STRING,
-# MAGIC 	FrenchEducation STRING,
-# MAGIC 	EnglishOccupation STRING,
-# MAGIC 	SpanishOccupation STRING,
-# MAGIC 	FrenchOccupation STRING,
-# MAGIC 	HouseOwnerFlag STRING,
-# MAGIC 	NumberCarsOwned STRING,
-# MAGIC 	AddressLine1 STRING,
-# MAGIC 	AddressLine2 STRING,
-# MAGIC 	Phone STRING,
-# MAGIC 	DateFirstPurchase STRING,
-# MAGIC 	CommuteDistance STRING
+# MAGIC 	EmailAddress STRING
+# MAGIC 	-- YearlyIncome STRING,
+# MAGIC 	-- TotalChildren STRING,
+# MAGIC 	-- NumberChildrenAtHome STRING,
+# MAGIC 	-- EnglishEducation STRING,
+# MAGIC 	-- SpanishEducation STRING,
+# MAGIC 	-- FrenchEducation STRING,
+# MAGIC 	-- EnglishOccupation STRING,
+# MAGIC 	-- SpanishOccupation STRING,
+# MAGIC 	-- FrenchOccupation STRING,
+# MAGIC 	-- HouseOwnerFlag STRING,
+# MAGIC 	-- NumberCarsOwned STRING,
+# MAGIC 	-- AddressLine1 STRING,
+# MAGIC 	-- AddressLine2 STRING,
+# MAGIC 	-- Phone STRING,
+# MAGIC 	-- DateFirstPurchase STRING,
+# MAGIC 	-- CommuteDistance STRING
 # MAGIC ) using DELTA
 
 # COMMAND ----------
 
 # MAGIC %sql
 # MAGIC
-# MAGIC DROP TABLE dimCustomer
+# MAGIC -- DROP TABLE dimCustomer;
+# MAGIC DROP TABLE silver_dimcustomer
 
 # COMMAND ----------
 
-bronze_product = (spark.readStream.format("cloudFiles")\
-                .option("cloudFiles.format", "csv")\
-                .option("cloudFiles.maxFilesPerTrigger", "1")\
-                .option("cloudFiles.inferColumnTypes", True)\
-                .option("cloudFiles.schemaLocation",'dbfs:/mnt/input/')\
-                .load("dbfs:/mnt/input/")
-)
-
-display(bronze_product)
+from pyspark.sql.functions import *
+from pyspark.sql.types import *
 
 # COMMAND ----------
 
-bronze_customer = spark.read.csv("dbfs:/mnt/input/dimCustomer.csv", header=True)
+spark.readStream.format("cloudFiles")\
+  .option("cloudFiles.format", "parquet")\
+  .option("cloudFiles.schemaLocation",'dbfs:/mnt/input/schema')\
+  .load("dbfs:/mnt/input/*.parquet")\
+  .drop(col("_rescued_data"))\
+  .writeStream.option("checkpointLocation", 'dbfs:/mnt/input/checkpoint')\
+  .table("dimCustomer")
 
-display(bronze_customer)
+                # .option("cloudFiles.inferColumnTypes", True)\
+                # .option("cloudFiles.maxFilesPerTrigger", "1")\
 
 # COMMAND ----------
 
-bronze_customer.write.format("delta").mode("append").save("dbfs:/user/hive/warehouse/dimcustomer")
+bronze_customer.printSchema()
 
 # COMMAND ----------
 
 # MAGIC %sql
 # MAGIC
-# MAGIC SELECT * FROM dimCustomer
+# MAGIC SELECT COUNT(*) FROM dimcustomer;
+# MAGIC
+# MAGIC -- SELECT * FROM dimCustomer
 
 # COMMAND ----------
 
 # MAGIC %md
 # MAGIC
 # MAGIC <font color='red'><b> 5. SILVER LAYER - transform data
-
-# COMMAND ----------
-
-from pyspark.sql.functions import *
-from pyspark.sql.types import *
 
 # COMMAND ----------
 
