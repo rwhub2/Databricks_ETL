dc51372e2885c0af8c6c4a590543fddbd6f5e37e
rwhub2 <rwhub2>
2023-05-03 08:55:20 +0000

    o

---
+++ Notebooks/2. Unity Catalog.py
@@ -0,0 +1,20 @@
+# Databricks notebook source
+# MAGIC %md
+# MAGIC
+# MAGIC ### Unity Catalog
+
+# COMMAND ----------
+
+# MAGIC %sql
+# MAGIC
+# MAGIC CREATE CATALOG IF NOT EXISTS medallion_architecture;
+
+# COMMAND ----------
+
+# MAGIC %sql
+# MAGIC
+# MAGIC SHOW CATALOGS
+
+# COMMAND ----------
+
+
